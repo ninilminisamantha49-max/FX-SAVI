d869a8c00ea6a41ddf6733e0e5c9d134d2fb2342
ninilminisamantha49-max <ninilminisamantha49@gmail.com>
2026-07-15 18:10:05 +0530

    Refactor login function and update market status logic

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-function login() {
-    alert("FX SAVI Login System - Coming Soon");
+ function login() {
+
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    if(email == "" || password == "") {
+
+        alert("Email සහ Password ඇතුළත් කරන්න");
+
+    } else {
+
+        document.getElementById("loginBox").style.display = "none";
+        document.getElementById("homeBox").style.display = "block";
+
+        document.getElementById("analysis").innerHTML =
+        "Gold Fundamental Analysis Loading...";
+
+        document.getElementById("signal").innerHTML =
+        "WAIT";
+
+    }
+
 }
 
 
 // Weekend Market Close System
 
-let today = new Date().getDay();
+let day = new Date().getDay();
 
-if (today === 0 || today === 6) {
-    document.getElementById("market").innerHTML =
-    "Gold Market Closed (Weekend)";
+if(day == 0 || day == 6){
+
+    document.getElementById("marketStatus").innerHTML =
+    "🟡 Gold Market Closed (Weekend)";
+
+    document.getElementById("analysis").innerHTML =
+    "සෙනසුරාදා සහ ඉරිදා Gold Market වසා ඇත.";
 
     document.getElementById("signal").innerHTML =
-    "No Signal Available";
+    "No Signal";
+
 }
-else {
-    document.getElementById("market").innerHTML =
-    "Gold Market Open";
+else{
+
+    document.getElementById("marketStatus").innerHTML =
+    "🟢 Gold Market Open";
+
 }
