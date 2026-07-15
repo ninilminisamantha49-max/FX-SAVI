03d21b91967a400e91b5bb23795fbe2181c091d7
ninilminisamantha49-max <ninilminisamantha49@gmail.com>
2026-07-15 17:43:29 +0530

    Implement weekend market close logic in script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function login() {
+    alert("FX SAVI Login System - Coming Soon");
+}
 
+
+// Weekend Market Close System
+
+let today = new Date().getDay();
+
+if (today === 0 || today === 6) {
+    document.getElementById("market").innerHTML =
+    "Gold Market Closed (Weekend)";
+
+    document.getElementById("signal").innerHTML =
+    "No Signal Available";
+}
+else {
+    document.getElementById("market").innerHTML =
+    "Gold Market Open";
+}
